0ab1c833cf109ca303e3fb4317421c3bb05c0dea
Arthur Kachmazov <boss.kachmaz@mail.ru>
2022-06-28 14:23:05 +0300

    init

---
+++ src/components/AppRouter.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {Route, Routes, Navigate} from "react-router-dom";
+import {privateRoutes, publicRoutes} from "../routes";
+import {CHAT_ROUTE, LOGIN_ROUTE} from "../utils/consts";
+
+const AppRouter = () => {
+    const user = true;
+    return user ?
+        (
+            <Routes>
+                {
+                    privateRoutes.map(({path, Component}) =>
+                        <Route path={path} element={Component}/>
+                    )
+                }
+                <Route path="*" element={<Navigate to={CHAT_ROUTE} replace />}
+                />
+            </Routes>
+        )
+        :
+        (
+            <Routes>
+                {
+                    publicRoutes.map(({path, Component}) =>
+                        <Route path={path} element={Component}/>
+                    )
+                }
+                <Route
+                    path="*"
+                    element={<Navigate to={LOGIN_ROUTE} replace />}
+                />
+            </Routes>
+        );
+};
+
+export default AppRouter;
